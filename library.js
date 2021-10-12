5f7430cd364c4fc8913b916a7a01839752aabe97
Perry <petreli1@ymail.com>
2021-10-12 13:07:34 +0100

    read checkbox now correctly checks if box is read of not

--- library.js
+++ library.js
@@ -22,7 +22,7 @@ submit.addEventListener('click', () => {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
-    const read = document.getElementById('read').value;
+    const read = document.getElementById('read').checked;
 
     const newBook = new book(title, author, pages, read)
     addBookToLibrary(newBook);
