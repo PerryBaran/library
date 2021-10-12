b155d436cdd137a113d60177f935529c10d99b79
Perry <petreli1@ymail.com>
2021-10-12 12:57:38 +0100

    added submit form so users can submit information about a book and store it in an array

--- library.js
+++ library.js
@@ -1,3 +1,7 @@
+const submit = document.getElementById('submit');
+
+let myLibrary = [];
+
 function book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -7,7 +11,21 @@ function book(title, author, pages, read) {
         info = title + ' by ' + author + ', ' + pages + ' pages' + ', ' + read;   
         return info;
     }
-}
+};
+
+function addBookToLibrary(newBook) {
+    myLibrary.push(newBook)
+    console.log(myLibrary)
+};
+
+submit.addEventListener('click', () => {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').value;
+
+    const newBook = new book(title, author, pages, read)
+    addBookToLibrary(newBook);
+});
 
 const theHobbit = new book('The Hobbit', 'J.R.R Tolkien', '295', 'not read');
-console.log(theHobbit.info())
