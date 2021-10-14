f72a31562b83ec0e34fd26b3ce60bddf9c2f0b7e
Perry <petreli1@ymail.com>
2021-10-14 13:39:44 +0100

    added edit and delete button

--- library.js
+++ library.js
@@ -1,5 +1,6 @@
 const add = document.getElementById('add');
 const submit = document.getElementById('submit');
+const edit = document.getElementById('edit');
 const bookShelf = document.getElementById('bookshelf');
 const closeForm = document.getElementById('close');
 
@@ -26,21 +27,61 @@ function displayLibrary() {
 
     for (i = 0; i < myLibrary.length; i++) {
         let info = document.createElement('div');
+
+        if (myLibrary[i].read) {
+            checkbox = '<input type="checkbox" checked>'
+        } else {
+            checkbox = '<input type="checkbox">'
+        }
         info.className = 'table';
-        info.innerHTML = '<div class="cell span2">' + myLibrary[i].title + '</div><div class="cell span2">' + myLibrary[i].author + '</div><div class="cell">' + 
-            myLibrary[i].pages + '</div><div class="cell">' + myLibrary[i].read + '</div>';
+        info.innerHTML = '<div class="cell span2">' + myLibrary[i].title + '</div><div class="cell span2">'
+                     + myLibrary[i].author + '</div><div class="cell">' 
+                     + myLibrary[i].pages + '</div><div class="cell">'
+                     + checkbox + '</div><div class="cell">'
+                     + '<button onclick="editBook(' + i + ')">EDIT</button></div><div class="cell">'
+                     + '<button onclick="delBook(' + i + ')">DELETE</button></div>'
+        
         bookShelf.appendChild(info);
     }
-}
+};
 
 function reset(parent){
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
+};
+
+function editBook(i){
+    document.getElementById('titleEdit').value = myLibrary[i].title;
+    document.getElementById('titleEdit').dataset.indexNumber = i;
+    document.getElementById('authorEdit').value = myLibrary[i].author;
+    document.getElementById('pagesEdit').value = myLibrary[i].pages;
+    document.getElementById('readEdit').checked = myLibrary[i].read;
+    document.querySelector('.edit').style.display = "flex";
+}
+
+function delBook(i) {
+    myLibrary.splice(i, 1);
+    displayLibrary();
+}
+
+function close(){
+    document.querySelector('.container').style.display = "none";
 }
 
 add.addEventListener('click', () => {
-    document.getElementById('container').style.display = "flex";
+    document.querySelector('.new').style.display = "flex";
+});
+
+edit.addEventListener('click', () => {
+    const i = document.getElementById('titleEdit').dataset.indexNumber;
+    myLibrary[i].title = document.getElementById('titleEdit').value ;
+    myLibrary[i].author = document.getElementById('authorEdit').value;
+    myLibrary[i].pages = document.getElementById('pagesEdit').value;
+    myLibrary[i].read = document.getElementById('readEdit').checked;
+
+    document.querySelector('.edit').style.display = "none";
+    displayLibrary()
 });
 
 submit.addEventListener('click', () => {
@@ -54,13 +95,20 @@ submit.addEventListener('click', () => {
     displayLibrary()
 });
 
+
 closeForm.addEventListener('click', () => {
-    document.getElementById('container').style.display = "none";
+   close();
+});
+
+document.querySelectorAll('.del').forEach(button => {
+    button.addEventListener('click', () => {
+        console.log('yo')
+    })
 })
 
-const theHobbit = new book('The Hobbit', 'J.R.R Tolkien', '295', 'not read');
-const theScrobbit = new book('The Scrobbit', 'J.R.R Tolkent', '69', 'read');
-const theBopit = new book('The Bopit', 'J.R.R Tokesome', '420', 'not read');
+const theHobbit = new book('The Hobbit', 'J.R.R Tolkien', '295', false);
+const theScrobbit = new book('The Scrobbit', 'J.R.R Tolkent', '69', true);
+const theBopit = new book('The Bopit', 'J.R.R Tokesome', '420', false);
 
 myLibrary.push(theHobbit)
 myLibrary.push(theScrobbit)
