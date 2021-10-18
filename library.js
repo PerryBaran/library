5fcedce6a5a207ba3f4dcb57bef9db614c693bc2
Perry <petreli1@ymail.com>
2021-10-18 12:58:23 +0100

    added buttons to allow sorting of the library

--- library.js
+++ library.js
@@ -8,6 +8,7 @@ function book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.date = new Date()
     this.info = function() {
         info = title + ' by ' + author + ', ' + pages + ' pages' + ', ' + read;   
         return info;
@@ -21,6 +22,7 @@ function addBookToLibrary(newBook) {
 
 function displayLibrary() {
     reset(bookShelf)
+    sort();
 
     for (i = 0; i < myLibrary.length; i++) {
         let info = document.createElement('div');
@@ -31,6 +33,7 @@ function displayLibrary() {
                      + myLibrary[i].author + '</div><div class="cell">' 
                      + myLibrary[i].pages + '</div><div class="cell">'
                      + '<input type="checkbox" onclick ="updateRead(' + i + ')"' + checkbox + '>' + '</div><div class="cell">'
+                     + myLibrary[i].date.toString().slice(4, 21)  + '</div><div class="cell">'
                      + '<button onclick="editBook(' + i + ')">EDIT</button></div><div class="cell">'
                      + '<button onclick="delBook(' + i + ')">DELETE</button></div>'
         
@@ -136,6 +139,87 @@ cancel.addEventListener('click', () => {
 closeForm.addEventListener('click', () => {
    close();
 });
+
+//sorting
+function byTitle() {
+    myLibrary.sort((a, b) => (a.title > b.title) ? 1: -1)
+}
+
+function byTitleReverse() {
+    myLibrary.sort((a, b) => (a.title > b.title) ? -1: 1)
+}
+
+function byAuthor() {
+    byTitle();
+    myLibrary.sort((a, b) => (a.author > b.author) ? 1: -1)
+}
+
+function byAuthorReverse() {
+    byTitle();
+    myLibrary.sort((a, b) => (a.author > b.author) ? -1: 1)
+}
+
+function byPages() {
+    byTitle();
+    myLibrary.sort((a, b) => (a.pages > b.pages) ? 1: -1)
+}
+
+function byPagesReverse() {
+    byTitle();
+    myLibrary.sort((a, b) => (a.pages > b.pages) ? -1: 1)
+}
+
+function byRead() {
+    byTitle();
+    myLibrary.sort((a, b) => (a.read > b.read) ? 1: -1)
+}
+
+function byReadReverse() {
+    byTitle();
+    myLibrary.sort((a, b) => (a.read > b.read) ? -1: 1)
+}
+
+function byDate() {
+    byTitle();
+    myLibrary.sort((a, b) => (a.date > b.date) ? 1: -1)
+}
+
+function byDateReverse() {
+    byTitle();
+    myLibrary.sort((a, b) => (a.date > b.date) ? -1: 1)
+}
+
+var sort = byDate;
+const sortInput = document.querySelectorAll('.sort')
+sortInput.forEach(button => {
+    button.addEventListener('click', () => {
+        if (!button.id) {
+            return
+        } else if (button.id === "title") {
+            sort = byTitle;
+        } else if (button.id === "titleReverse") {
+            sort = byTitleReverse;
+        } else if (button.id === "author") {
+            sort = byAuthor;
+        } else if (button.id === "authorReverse") {
+            sort = byAuthorReverse;
+        } else if (button.id === "pages") {
+            sort = byPages;
+        } else if (button.id === "pagesReverse") {
+            sort = byPagesReverse;
+        } else if (button.id === "checked") {
+            sort = byRead;
+        } else if (button.id === "checkedReverse") {
+            sort = byReadReverse;
+        } else if (button.id === "date") {
+            sort = byDate;
+        } else if (button.id === "dateReverse") {
+            sort = byDateReverse;
+        }
+        displayLibrary();
+    });
+});
+   
 
 const theHobbit = new book('The Hobbit', 'J.R.R Tolkien', '295', false);
 const theScrobbit = new book('The Scrobbit', 'J.R.R Tolkent', '69', true);
